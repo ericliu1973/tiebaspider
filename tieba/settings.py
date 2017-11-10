aafa8cb7cdffc56e542f1035a59c286b608688a2
duolaAOA <1219960386@qq.com>
2017-11-10 19:25:05 +0800

    add

--- tieba/settings.py
+++ tieba/settings.py
@@ -42,10 +42,9 @@ CONCURRENT_REQUESTS = 32
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+'Referer': 'https://www.baidu.com/',
+}
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
